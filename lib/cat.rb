e6ff68446946ec433fe5aca2708554ed88102758
Loren Cipriano <loren25cipriano@gmail.com>
2020-05-02 01:57:10 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,11 +1,25 @@
+
+# Cat
+#   Instance methods
+#     initializes with a name and an Owner
+#     can change its owner
+#     can't change its name
+#     initializes with a nervous mood
+#     can change its mood
+
 class Cat
   attr_reader :name
   attr_accessor :owner, :mood
+
+  @@all = []
 
   def initialize(name, owner, mood = "nervous")
     @name = name
     @owner = owner
     @mood = mood
   end
+
+  def self.all 
+  end 
 
 end
