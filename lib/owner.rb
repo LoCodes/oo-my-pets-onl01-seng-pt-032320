579238a7147196f5403d76a6798a959e572c0aa6
Loren Cipriano <loren25cipriano@gmail.com>
2020-05-02 01:38:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,18 @@
+# Owner
+#   Instance methods
+#     #name
+#       can have a name (FAILED - 1)
+#       cannot change owner's name (FAILED - 2)
+#     #species
+#       initializes with species set to human (FAILED - 3)
+#       can't change its species (FAILED - 4)
+#     #say_species
+#       can say its species (FAILED - 5)
+#   Class methods
+#     .all returns all instances of Owner that have been created (FAILED - 6)
+#     .count returns the number of owners that have been created (FAILED - 7)
+#     .reset_all can reset the owners that have been created
+
 class Owner
-  # code goes here
+  attr_accessor :name 
 end
